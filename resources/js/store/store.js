858a33646d9e94ad19512b6b1e8a30c15567d4dc
Saif Hatem <saifxhatem@gmail.com>
2021-01-28 14:02:23 +0200

    Added more methods to store

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -4,17 +4,20 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {
-        
+    state: {   
         user:
             {
                 user_id: null,
                 user_name: null,
                 status: 0,
-
+                favorite_count: null
             },
         router: null,
-        articles: null
+        articles: null,
+        favorites: null,
+        flags: {
+            no_favorites_in_selected_section: null
+        }
     },
     getters: {
         
@@ -37,7 +40,23 @@ export const store = new Vuex.Store({
         },
         clear_articles (state) {
             state.articles = []
-        }
+        },
+        set_favorites (state, payload) {
+            state.favorites = payload
+        },
+        clear_favorites (state) {
+            state.favorites = []
+        },
+        set_user_favorites_count (state, payload) {
+            state.user.favorite_count = payload
+        },
+        set_no_articles_flag (state, payload) {
+            if (payload == true)
+                state.flags.no_favorites_in_selected_section = true
+            if (payload == false)
+            state.flags.no_favorites_in_selected_section = false
+            
+        },
     },
     actions: {
         do_login ({ commit, state, dispatch }, formData)
@@ -108,7 +127,62 @@ export const store = new Vuex.Store({
         clear_articles ({commit, state})
         {
             commit('clear_articles')
-        }
+        },
+        load_favorites ({commit, state}, query)
+        {
+            //console.log("In action: Query = " + query.payload.filter)
+            axios.post('load-favorites', { user_id: query.payload.user_id, filter: query.payload.filter })
+                .then((response) => {
+                    if (response.status === 216) //no articles
+                        commit('set_no_articles_flag', true)
+                    if (response.data && response.status != 216) //check existence of data before assigning
+                    {                        
+                        commit('set_favorites', response.data)
+                        commit('set_no_articles_flag', false)
+                    }
+                        
+                })
+                .catch(function(error) {});
+        },
+        save_favorites ({commit, state}, query)
+        {
+            axios.post('add-to-favorites', query.payload)
+                .then((response) => {
+                    if (response.status = 200) //check existence of data before assigning
+                        console.log("Article saved successfully")
+                })
+                .catch(function(error) {});
+        },
+        delete_favorites ({commit, state}, query)
+        {
+            axios.post('delete-favorite', {
+                //send the request to delete the article the user wants to remove
+                user_id: query.payload.user_id,
+                article_id: query.payload.article_id
+            })
+            .then((response) => {
+                if (response.status = 200) //check existence of data before assigning
+                    console.log("Article deleted successfully")
+            })
+            .catch(function(error) {});
+        },
+        clear_favorites ({commit, state})
+        {
+            commit('clear_favorites')
+        },
+        get_user_favorite_count ({commit, state}, query)
+        {
+            axios.post('get-favorite-count', {
+                //send the request to delete the article the user wants to remove
+                user_id: query.payload.user_id,
+            })
+            .then((response) => {
+                console.log("Count = " + response.data)
+                commit('set_user_favorites_count', response.data)
+            })
+            .catch(function(error) {});
+            
+        },
     }
     
 })
